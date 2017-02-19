d977ce673da5c933df7e7c31aa2c654750a1dde9
Kat Hicks <kmhicks92@gmail.com>
2017-02-19 19:58:02 +0000

    Added isotope filtering

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$( document ).ready(function() {
+$(window).load(function(){
 
   $(".portfolio-item").mouseover(function(){
         $(this).css("border-color", "#2c3e50");
@@ -7,6 +7,34 @@ $( document ).ready(function() {
     $(".portfolio-item").mouseout(function(){
         $(this).css("border-color", "#f1f1f1");
         $(this).children(".portfolio-image").fadeTo(80, 1);
+    });
+
+
+    var $container = $('.portfolioContainer');
+    console.log($container);
+    $container.isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 750,
+            easing: 'linear',
+            queue: false
+        }
+    });
+    $('.portfolioFilter a').click(function(){
+        console.log($('.portfolioFilter a'));
+        $('.portfolioFilter .current').removeClass('current');
+        $(this).addClass('current');
+
+        var selector = $(this).attr('data-filter');
+        $container.isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+         });
+         return false;
     });
 
 });
